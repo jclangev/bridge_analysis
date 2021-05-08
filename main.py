86fc232b775d118e5a55280daa39b8d50a323d92
jclangev <33791718+jclangev@users.noreply.github.com>
2021-05-08 11:19:26 +0200

    added our score

--- main.py
+++ main.py
@@ -12,9 +12,15 @@ from pprint import pprint
 from scrape_double_dummy import get_optimal_points_for_deal
 
 import requests
+import bs4
 from bs4 import BeautifulSoup
 
-from scrape_stepbridge import get_board_id, get_board_double_dummy_url, get_board_result_dicts
+import scrape_stepbridge
+
+
+def convert_dutch_percentage_string_to_float(text: str) -> float:
+    return float(text.replace('%', '').replace(',', '.'))
+
 
 step_url = 'https://results.stepbridge.nl/tournament/events/show/38602/JoostL'
 print('step_url:', step_url)
@@ -24,13 +30,19 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 board_tags = soup.find_all('table', class_='board')
 for i, board_tag in enumerate(board_tags[0:1]):
-    board_id = get_board_id(board_tag)
-    optimal_points = get_optimal_points_for_deal(get_board_double_dummy_url(board_tag))
-    other_result_dicts = get_board_result_dicts(board_tag, 'fieldrow')
-    our_result_dict = get_board_result_dicts(board_tag, 'fieldrowselected')[0]
+    board_id = scrape_stepbridge.get_board_id(board_tag)
+    optimal_points = get_optimal_points_for_deal(scrape_stepbridge.get_board_double_dummy_url(board_tag))
+    other_result_dicts = scrape_stepbridge.get_board_result_dicts(board_tag, 'fieldrow')
+    our_result_dict = scrape_stepbridge.get_board_result_dicts(board_tag, 'fieldrowselected')[0]
     other_points = [int(fieldrow_dict.get('punten')) for fieldrow_dict in other_result_dicts]
     our_points = int(our_result_dict.get('punten'))
+    our_score = convert_dutch_percentage_string_to_float(our_result_dict.get('score'))
 
     print('other points:', other_points)
     print('our points:', our_points)
     print('optimal points:', optimal_points)
+
+    print('our score:', our_score)
+
+
+
--- scrape_stepbridge.py
+++ scrape_stepbridge.py
@@ -11,6 +11,13 @@ from collections import OrderedDict
 import bs4
 
 
+def get_clean_text_with_img_replaced_by_alt(tag: bs4.element.Tag) -> str:
+    if tag.findChild('img'):
+        suit = tag.find('img').get('alt')
+        tag.find('img').replace_with(suit)
+    return tag.text.replace('\n', '').strip()
+
+
 def get_board_id(board_tag: bs4.element.Tag) -> str:
     result = board_tag.findChild('th', {'class': 'boardheaderleft'}).text
     result = result.replace('\n', '').replace('            ', ' ')
@@ -24,21 +31,31 @@ def get_board_double_dummy_url(board_tag: bs4.element.Tag) -> str:
 
 
 def get_board_result_dicts(board_tag: bs4.element.Tag, fieldrow_class_name: str) -> list:
-    def get_field_row_column_value(fieldrow_column: bs4.element.Tag) -> str:
-        if fieldrow_column.findChild('img'):
-            return fieldrow_column.text + fieldrow_column.findChild('img').get('alt')
-        else:
-            return fieldrow_column.text.replace('\n', '')
-
     def get_fieldrow_dict(field_row: bs4.element.Tag) -> dict:
         keys = ['leider', 'contract', 'resultaat', 'door', 'uitkomst', 'punten', 'score']
         result_fieldrow_dict = OrderedDict([])
         fieldrow_columns = field_row.findChildren('td')
         for fieldrow_column, key in zip(fieldrow_columns, keys):
-            result_fieldrow_dict[key] = get_field_row_column_value(fieldrow_column)
+            result_fieldrow_dict[key] = get_clean_text_with_img_replaced_by_alt(fieldrow_column)
         return result_fieldrow_dict
 
     result = []
     for fieldrow in board_tag.find_all(class_=fieldrow_class_name):
         result.append(get_fieldrow_dict(fieldrow))
     return result
+
+
+def get_our_result(board_tag: bs4.element.Tag) -> str:
+    board_tables = board_tag.findChild('tbody').find('tr').find('td').find_all('table')
+    our_result_rows = board_tables[2].find_all('tr')
+    our_contract_points_tag = our_result_rows[0].find_all('td')[1]
+    result_string = get_clean_text_with_img_replaced_by_alt(our_contract_points_tag)
+    return result_string
+
+
+def get_our_score(board_tag: bs4.element.Tag) -> str:
+    board_tables = board_tag.findChild('tbody').find('tr').find('td').find_all('table')
+    our_result_rows = board_tables[2].find_all('tr')
+    our_score_tag = our_result_rows[1].find_all('td')[1]
+    score_string = get_clean_text_with_img_replaced_by_alt(our_score_tag)
+    return score_string
