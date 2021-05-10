12283921baf8c6f5bfe2bda36e30f5d82e7680bd
jclangev <33791718+jclangev@users.noreply.github.com>
2021-05-10 21:29:49 +0200

    refactoring - dds analysis scrape working again

--- main.py
+++ main.py
@@ -11,13 +11,10 @@ import os
 import numpy as np
 import requests
 import bs4
-import util
 
-# from scrape_double_dummy import get_optimal_points_for_deal
-# from scrape_stepbridge import get_board_chair_dict
 import scrape_stepbridge
 import scrape_double_dummy
-# from util import convert_dutch_percentage_string_to_float, calculate_mp_score
+import util
 
 stepbridge_personal_tournament_url = 'https://results.stepbridge.nl/tournament/events/show/38602/JoostL'
 my_name = os.path.basename(stepbridge_personal_tournament_url)
@@ -33,47 +30,49 @@ board_tags = soup.find_all('table', class_='board')
 i = 16
 a_board_tag = board_tags[i]
 
-board_id = scrape_stepbridge.get_board_id(a_board_tag)
-
-other_result_dicts = scrape_stepbridge.get_board_result_dicts(a_board_tag, 'fieldrow')
-our_result_dict = scrape_stepbridge.get_board_result_dicts(a_board_tag, 'fieldrowselected')[0]
-
-other_points = [int(fieldrow_dict.get('punten')) for fieldrow_dict in other_result_dicts]
-our_points = int(our_result_dict.get('punten'))
-board_chair_dict = scrape_stepbridge.get_board_chair_dict(a_board_tag)
-flip_optimal_points = (board_chair_dict.get(my_name).upper()) in ['O', 'W']
+# board_id = scrape_stepbridge.get_board_id(a_board_tag)
+#
+# other_result_dicts = scrape_stepbridge.get_board_result_dicts(a_board_tag, 'fieldrow')
+# our_result_dict = scrape_stepbridge.get_board_result_dicts(a_board_tag, 'fieldrowselected')[0]
+#
+# other_points = [int(fieldrow_dict.get('punten')) for fieldrow_dict in other_result_dicts]
+# our_points = int(our_result_dict.get('punten'))
+# board_chair_dict = scrape_stepbridge.get_board_chair_dict(a_board_tag)
+# flip_optimal_points = (board_chair_dict.get(my_name).upper()) in ['O', 'W']
 
 double_dummy_url = scrape_stepbridge.get_board_double_dummy_url(a_board_tag)
 print(double_dummy_url)
 
-dds_query_url = scrape_double_dummy.get_double_dummy_analysis_bidding_query_url(double_dummy_url)
-print(dds_query_url)
 
-double_dummy_analysis_dict = scrape_double_dummy.get_double_dummy_analysis_dict_for_deal(dds_query_url)
-print(double_dummy_analysis_dict)
+def get_lead_from_double_dummy_url(double_dummy_url: str) -> str:
+    return {}
 
-lead_analysis_parameters = '&trumps=S&leader=n'
-dds_lead_query_url = dds_query_url.replace('request=m', 'request=g').replace('W:', '') + lead_analysis_parameters
+
+dds_bidding_query_url = scrape_double_dummy.get_double_dummy_analysis_bidding_query_url(double_dummy_url)
+print(dds_bidding_query_url)
+dds_bidding_analysis_dict = scrape_double_dummy.get_double_dummy_analysis_dict_for_deal(dds_bidding_query_url)
+print(dds_bidding_analysis_dict)
+
+dds_lead_query_url = scrape_double_dummy.get_double_dummy_analysis_lead_query_url(double_dummy_url)
 print(dds_lead_query_url)
-
 dds_lead_analysis_dict = scrape_double_dummy.get_double_dummy_analysis_dict_for_deal(dds_lead_query_url)
 print(dds_lead_analysis_dict)
 
 # optimal_points_raw = scrape_double_dummy.get_optimal_points_for_deal(scrape_stepbridge.get_board_double_dummy_url(a_board_tag))
 # optimal_points = -optimal_points_raw if flip_optimal_points else optimal_points_raw
 #
-our_score = util.convert_dutch_percentage_string_to_float(our_result_dict.get('score'))
+# our_score = util.convert_dutch_percentage_string_to_float(our_result_dict.get('score'))
 # optimal_score = util.calculate_mp_score(other_points + [optimal_points], optimal_points)
 # possible_gain = max(0.0, optimal_score - our_score)
 
 
-print(f'{board_id:16} '
-      + f'our points: {our_points:5}  '
-      # + f'optimal points: {optimal_points:5}  '
-      + f'board {i + 1:2}:  our score: {our_score:6.2f}  '
-      # + f'optimal score: {optimal_score:6.2f}  '
-      # + f'possible gain: {possible_gain:6.2f}'
-      )
+# print(f'{board_id:16} '
+#       + f'our points: {our_points:5}  '
+#       # + f'optimal points: {optimal_points:5}  '
+#       + f'board {i + 1:2}:  our score: {our_score:6.2f}  '
+#       + f'optimal score: {optimal_score:6.2f}  '
+#       + f'possible gain: {possible_gain:6.2f}'
+#       )
 
 # our_total_score = np.mean(our_scores)
 # optimal_total_score = np.mean(optimal_scores)
@@ -85,7 +84,6 @@ print(f'{board_id:16} '
 # print(f'possible max score:  {our_total_score + total_possible_gain:6.2f}')
 print()
 print('done')
-
 
 # overview_url = 'https://portal.stepbridge.nl/tournament/events/index/users/74285'
 # print('overview_url:', overview_url)
--- scrape_double_dummy.py
+++ scrape_double_dummy.py
@@ -10,102 +10,96 @@ import json
 import urllib.request
 
 
-PARAMETER_SEPARATOR = '&'
-KEY_VALUE_SEPARATOR = '='
+DDS_QUERY_URL_PREFIX = 'https://dds.bridgewebs.com/cgi-bin/bsol2/ddummy?'
+DDS_QUERY_PARAMETER_SEPARATOR = '&'
+DDS_QUERY_KEY_VALUE_SEPARATOR = '='
 
 
 def convert_dds_bridgewebs_com_url_to_parameter_dict(dds_bridgewebs_com_url: str) -> dict:
-    parameter_separator = '&'
-    key_value_separator = '='
     return {
-        pair.split(key_value_separator)[0]: pair.split(key_value_separator)[1]
-        for pair in dds_bridgewebs_com_url.split(parameter_separator)
+        pair.split(DDS_QUERY_KEY_VALUE_SEPARATOR)[0]: pair.split(DDS_QUERY_KEY_VALUE_SEPARATOR)[1]
+        for pair in dds_bridgewebs_com_url.split(DDS_QUERY_PARAMETER_SEPARATOR)
     }
+
+
+def make_lead_deal_str(parameter_dict: dict) -> str:
+    deal_string_key = 'dealstr'
+    hand_separator = 'x'
+    hands_keys = ['west', 'north', 'east', 'south']
+    deal_str_result = DDS_QUERY_PARAMETER_SEPARATOR + deal_string_key
+    deal_str_result += DDS_QUERY_KEY_VALUE_SEPARATOR + parameter_dict.get(hands_keys[0])
+    for hand_key in hands_keys[1:]:
+        deal_str_result += hand_separator + parameter_dict.get(hand_key)
+    return deal_str_result
+
+
+def make_bidding_deal_str(parameter_dict: dict) -> str:
+    hand_separator = 'x'
+    hands_keys = ['west', 'north', 'east', 'south']
+    deal_str_result = DDS_QUERY_PARAMETER_SEPARATOR + 'dealstr' + DDS_QUERY_KEY_VALUE_SEPARATOR
+    deal_str_result += 'W:' + parameter_dict.get(hands_keys[0])
+    for hand_key in hands_keys[1:]:
+        deal_str_result += hand_separator + parameter_dict.get(hand_key)
+    return deal_str_result
+
+
+def declarer_to_leader_str(parameter_dict: dict) -> str:
+    declarer_key = 'declarer'
+    leader_key = 'leader'
+    declarer_to_leader_translation_dict = {'W': 'n', 'N': 'o', 'O': 'z', 'Z': 'w'}
+    leader_str_result = DDS_QUERY_PARAMETER_SEPARATOR + leader_key + DDS_QUERY_KEY_VALUE_SEPARATOR
+    leader_str_result += declarer_to_leader_translation_dict.get(parameter_dict.get(declarer_key))
+    return leader_str_result
+
+
+def contract_to_trumps_str(parameter_dict: dict) -> str:
+    trump_key = 'trumps'
+    contract_key = 'contract'
+    contract = parameter_dict.get(contract_key)
+    trumps = contract[-1]
+    return DDS_QUERY_PARAMETER_SEPARATOR + trump_key + DDS_QUERY_KEY_VALUE_SEPARATOR + trumps
+
+
+def copied_parameters_str(parameter_dict: dict) -> str:
+    key_value_keys = ['vul']
+    parameters_str_result = ''
+    for key in key_value_keys:
+        parameters_str_result += DDS_QUERY_PARAMETER_SEPARATOR + key
+        parameters_str_result += DDS_QUERY_KEY_VALUE_SEPARATOR + parameter_dict.get(key)
+    return parameters_str_result
 
 
 def get_double_dummy_analysis_bidding_query_url(double_dummy_analysis_url: str) -> str:
     """"For bidding analysis"""
-    def make_bidding_deal_str(parameter_dict: dict) -> str:
-        deal_string_prefix = 'W:'
-        hand_separator = 'x'
-        parameter_separator = '&'
-        key_value_separator = '='
-
-        hands_keys = ['west', 'north', 'east', 'south']
-        deal_str_result = deal_string_prefix + parameter_dict.get(hands_keys[0])
-        for hand_key in hands_keys[1:]:
-            deal_str_result += hand_separator + parameter_dict.get(hand_key)
-
-        key_value_keys = ['vul']
-        for key in key_value_keys:
-            deal_str_result += parameter_separator + key + key_value_separator + parameter_dict.get(key)
-
-        return deal_str_result
-
-    dds_url_prefix = 'https://dds.bridgewebs.com/cgi-bin/bsol2/ddummy?request=m&dealstr='
-    dds_url_deal_str = make_bidding_deal_str(convert_dds_bridgewebs_com_url_to_parameter_dict(double_dummy_analysis_url))
-    return dds_url_prefix + dds_url_deal_str
+    url_parameter_dict = convert_dds_bridgewebs_com_url_to_parameter_dict(double_dummy_analysis_url)
+    dds_url = DDS_QUERY_URL_PREFIX + 'request' + DDS_QUERY_KEY_VALUE_SEPARATOR + 'm'
+    dds_url += make_bidding_deal_str(url_parameter_dict)
+    dds_url += copied_parameters_str(url_parameter_dict)
+    return dds_url
 
 
 def get_double_dummy_analysis_lead_query_url(double_dummy_analysis_url: str) -> str:
     """"For lead analysis"""
-
-    def make_lead_deal_str(parameter_dict: dict) -> str:
-        deal_string_key = 'dealstr'
-        hand_separator = 'x'
-        hands_keys = ['west', 'north', 'east', 'south']
-        deal_str_result = parameter_separator + deal_string_key
-        deal_str_result += key_value_separator + parameter_dict.get(hands_keys[0])
-        for hand_key in hands_keys[1:]:
-            deal_str_result += hand_separator + parameter_dict.get(hand_key)
-
-        return deal_str_result
-
-    def declarer_to_leader_str(parameter_dict: dict) -> str:
-        declarer_key = 'declarer'
-        leader_key = 'leader'
-        declarer_to_leader_translation_dict = {'W': 'n', 'N': 'o', 'O': 'z', 'Z': 'w'}
-        leader_str_result = parameter_separator + leader_key + key_value_separator
-        leader_str_result += declarer_to_leader_translation_dict.get(parameter_dict.get(declarer_key))
-        return leader_str_result
-
-    def contract_to_trumps_str(parameter_dict: dict) -> str:
-        trump_key = 'trumps'
-        contract_key = 'contract'
-        contract = parameter_dict.get(contract_key)
-        trumps = contract[-1]
-        return parameter_separator + trump_key + key_value_separator + trumps
-
-    def copied_parameters_str(parameter_dict: dict) -> str:
-        key_value_keys = ['vul']
-        parameters_str_result = ''
-        for key in key_value_keys:
-            parameters_str_result += parameter_separator + key + key_value_separator + parameter_dict.get(key)
-        return parameters_str_result
-
     url_parameter_dict = convert_dds_bridgewebs_com_url_to_parameter_dict(double_dummy_analysis_url)
-
-    dds_url = 'https://dds.bridgewebs.com/cgi-bin/bsol2/ddummy'
+    dds_url = DDS_QUERY_URL_PREFIX + 'request' + DDS_QUERY_KEY_VALUE_SEPARATOR + 'g'
     dds_url += make_lead_deal_str(url_parameter_dict)
     dds_url += declarer_to_leader_str(url_parameter_dict)
     dds_url += contract_to_trumps_str(url_parameter_dict)
-    dds_url += copied_parameters_str
-
+    dds_url += copied_parameters_str(url_parameter_dict)
     return dds_url
 
 
-def get_double_dummy_analysis_dict_for_deal(double_dummy_analysis_url: str) -> int:
+def get_double_dummy_analysis_dict_for_deal(double_dummy_analysis_url: str) -> dict:
     """"
     takes URL for Double Dummy Solver at dds.bridgewebs.com and returns optimal score for deal in that URL
     """
     response = urllib.request.urlopen(double_dummy_analysis_url)
     response_string = response.read().decode('utf-8')
     response_dict = json.loads(response_string)
-
     return response_dict
 
 
-def get_optimal_points_from_double_dummy_analysis_dict(double_dummy_analysis_dict: dict) -> dict:
+def get_optimal_points_from_double_dummy_analysis_dict(double_dummy_analysis_dict: dict) -> int:
     optimal_points_key = 'scoreNS'
     optimal_points_prefix = 'NS '
     optimal_points_raw_string = double_dummy_analysis_dict.get(optimal_points_key, optimal_points_prefix)
